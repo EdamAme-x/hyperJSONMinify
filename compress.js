86c3016665b6d935eaf8618f525eb4c08e16c887
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-07 17:28:20 +0900

    Create compress.js

---
+++ compress.js
@@ -0,0 +1,57 @@
+function compressObject(obj) {
+  const result = Object.keys(obj).map(key => {
+    const value = obj[key];
+    let compressedValue;
+
+    if (Array.isArray(value) && value.length === 0) {
+      compressedValue = '%'; 
+    } else if (value === "") {
+      compressedValue = '*';
+    } else if (value === '') {
+      compressedValue = '+'; 
+    } else if (typeof value === 'object' && Object.keys(value).length === 0) {
+      compressedValue = '@';
+    } else if (typeof value === 'object' && Object.keys(value).length > 0) {
+      const innerStr = compressObject(value);
+      compressedValue = `|${innerStr}|`;
+    } else {
+      compressedValue = JSON.stringify(value);
+    }
+
+    return `${key}:${compressedValue}`;
+  }).join(',');
+
+  return result;
+}
+
+export default function HyperJSONMinify(obj) {
+  return reverseString(compressObject)
+}
+
+// function calcRatio(original, compressed) {
+//   const originalLength = original.length;
+//   const compressedLength = compressed.length;
+//   const ratio = (compressedLength / originalLength) * 100;
+//   return ratio.toFixed(2);
+// }
+//
+// 例
+// const obj = {
+//   "a": "a",
+//   "b": '',
+//   "c": 0,
+//   "d": {
+//     "r": true
+//   },
+//   "k": "",
+//   "arr": [],
+//   "str1": "",
+//   "str2": ''
+// };
+
+// const originalStr = JSON.stringify(obj);
+// const compressedStr = compressObject(obj);
+
+// console.log("Original String:", originalStr);
+// console.log("Compressed String:", compressedStr);
+// console.log("Compression Ratio:", calcRatio(originalStr, compressedStr) + "%");
