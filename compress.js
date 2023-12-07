8bd51397222fba44591758a51386577f9a686ea0
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-07 17:32:30 +0900

    Update compress.js

--- compress.js
+++ compress.js
@@ -28,6 +28,10 @@ function compressObject(obj) {
   return result;
 }
 
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
 export default function HyperJSONMinify(obj) {
   return reverseString(compressObject(obj))
 }
